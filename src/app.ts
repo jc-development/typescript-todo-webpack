7101da265628764081814141d09df25d9f803bfb
jc-development <joe.cardella3@gmail.com>
2022-03-19 16:35:18 -0400

    init commit

---
+++ src/app.ts
@@ -0,0 +1,9 @@
+import { ProjectInput } from './components/project-input';
+import { ProjectList } from './components/project-list';
+import '../app.css'
+
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
+
+console.log('hi')
